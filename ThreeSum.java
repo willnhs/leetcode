8f3ea6b8a16368beb32dd100db55fcbf27d12fd7
willnhs <95152342+willnhs@users.noreply.github.com>
2025-07-25 14:21:07 -0300

    Optimized solution to be O(n^2) time

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,64 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                for (int k = j + 1; k < nums.length; k++){
-                    if (nums[i] + nums[j] + nums[k] == 0){
-                        ArrayList<Integer> triplet = new ArrayList<>();
-                        
-                        /*
-                        // ijk
-                        if (nums[i] <= nums[j] && nums[j] <= nums[k] && nums[i] <= nums[k]) {
-                            triplet.add(nums[i]);
-                            triplet.add(nums[j]);
-                            triplet.add(nums[k]);
-                        }
-                        // ikj
-                        else if (nums[i] <= nums[k] && nums[k] <= nums[j] && nums[i] <= nums[j]){
-                            triplet.add(nums[i]);
-                            triplet.add(nums[k]);
-                            triplet.add(nums[j]);
-                        }
-                        // jik
-                        else if (nums[j] <= nums[i] && nums[i] <= nums[k] && nums[j] <= nums[k]){
-                            triplet.add(nums[j]);
-                            triplet.add(nums[i]);
-                            triplet.add(nums[k]);
-                        }
-                        // jki
-                        else if (nums[j] <= nums[k] && nums[k] <= nums[i] && nums[j] <= nums[i]) {
-                            triplet.add(nums[j]);
-                            triplet.add(nums[k]);
-                            triplet.add(nums[i]);
-                        }
-                        // kji
-                        else if (nums[k] <= nums[j] && nums[j] <= nums[i] && nums[k] <= nums[i]) {
-                            triplet.add(nums[k]);
-                            triplet.add(nums[j]);
-                            triplet.add(nums[i]);
-                        }
-                        // kij
-                        else if (nums[k] <= nums[i] && nums[i] <= nums[j] && nums[k] <= nums[j]) {
-                            triplet.add(nums[k]);
-                            triplet.add(nums[i]);
-                            triplet.add(nums[j]);
-                        }
-                        */
+            int target = -nums[i];
 
-                        triplet.add(nums[i]);
-                        triplet.add(nums[j]);
-                        triplet.add(nums[k]);
-                        Collections.sort(triplet);
-                        if (!triplets.contains(triplet)) {
-                            triplets.add(triplet);
-                        }  
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                List<Integer> triplet = new ArrayList<>();
+                int sum = nums[left] + nums[right];
+                if (i == left || sum < target){
+                    ++left;
+                }
+                else if (i == right || sum > target){
+                    --right;
+                }
+                else {
+                    triplet.add(nums[i]);
+                    triplet.add(nums[left]);
+                    triplet.add(nums[right]);
+                    if (!triplets.contains(triplet)){
+                        triplets.add(triplet);
                     }
+                    ++left;
                 }
             }
         }
-        
         return triplets;
     }
 }
