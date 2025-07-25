5d6e767f76f5512c5362fd78afef61a8925bf945
willnhs <95152342+willnhs@users.noreply.github.com>
2025-07-25 09:31:03 -0300

    Leetcode #100

--- TreeNode.java
+++ TreeNode.java
@@ -60,7 +60,13 @@ public class TreeNode{
     return Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(root.left) && isBalanced(root.right);
   }
 
-  public static void main(String[] args){
-
+  public boolean isSameTree(TreeNode p, TreeNode q){
+    if (p == null && q == null){
+      return true;
+    }
+    if (p != null && q != null && p.val == q.val){
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    return false;
   }
 }
